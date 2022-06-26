afaecc907678861656f86ea711dfa85e39f57877
Manuel Guevara <thonyguev-github@outlook.com>
2022-06-26 01:15:38 -0500

    removing "tensorflow.keras" leaving only "keras" added gui with tkinter the "while" loop was commented, which allows chatting with the bot in the console, to pass the functionality to the GUI

--- chatgui.py
+++ chatgui.py
@@ -1,37 +1,47 @@
-import nltk, json, random, pickle
+from keras.models import load_model
+import numpy as np
+from tkinter import *
+import pickle
+import nltk
+import json
+import random
+import pickle
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
-import pickle
-import numpy as np
-from tensorflow.keras.models import load_model
 model = load_model('chatbot_model.h5')
 intents = json.loads(open('intents.json').read())
-words = pickle.load(open('words.pkl','rb'))
-classes = pickle.load(open('classes.pkl','rb'))
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
 
 # preprocessamento input utente
+
+
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    sentence_words = [lemmatizer.lemmatize(
+        word.lower()) for word in sentence_words]
     return sentence_words
 
 # creazione bag of words
+
+
 def bow(sentence, words, show_details=True):
     sentence_words = clean_up_sentence(sentence)
     bag = [0]*len(words)
     for s in sentence_words:
-        for i,w in enumerate(words):
+        for i, w in enumerate(words):
             if w == s:
                 bag[i] = 1
                 if show_details:
-                    print ("found in bag: %s" % w)
+                    print("found in bag: %s" % w)
     return(np.array(bag))
 
+
 def calcola_pred(sentence, model):
-    p = bow(sentence, words,show_details=False)
+    p = bow(sentence, words, show_details=False)
     res = model.predict(np.array([p]))[0]
     ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
@@ -39,20 +49,23 @@ def calcola_pred(sentence, model):
         return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
+
 def getRisposta(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
-        if(i['tag']== tag):
+        if(i['tag'] == tag):
             result = random.choice(i['responses'])
             break
     return result
+
 
 def inizia(msg):
     ints = calcola_pred(msg, model)
     res = getRisposta(ints, intents)
     return res
 
+'''
 utente = ''
 print('Benvenuto! Per uscire, scrivi "Esci"')
 
@@ -60,3 +73,46 @@ while utente != 'esci':
     utente = str(input(""))
     res = inizia(utente)
     print('AI:' + res)
+'''
+
+# Creating GUI with tkinter
+
+
+def send():
+    msg = EntryBox.get("1.0", 'end-1c').strip()
+    EntryBox.delete("0.0", END)
+    if msg != '':
+        ChatLog.config(state=NORMAL)
+        print(f"You: {msg}")
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="#442265", font=("Verdana", 12))
+        # res = chatbot_response(msg)
+        res = inizia(msg)
+        print(f"Bot: {res}")
+        ChatLog.insert(END, "Bot: " + res + '\n\n')
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
+
+
+base = Tk()
+base.title("Hello")
+base.geometry("400x500")
+base.resizable(width=FALSE, height=FALSE)
+# Create Chat window
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial")
+ChatLog.config(state=DISABLED)
+# Bind scrollbar to Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+ChatLog['yscrollcommand'] = scrollbar.set
+# Create Button to send message
+SendButton = Button(base, font=("Verdana", 12, 'bold'), text="Send", width="12",
+                    height=5, bd=0, bg="#32de97", activebackground="#3c9d9b", fg='#ffffff', command=send)
+# Create the box to enter message
+EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
+# EntryBox.bind("<Return>", send)
+# Place all components on the screen
+scrollbar.place(x=376, y=6, height=386)
+ChatLog.place(x=6, y=6, height=386, width=370)
+EntryBox.place(x=128, y=401, height=90, width=265)
+SendButton.place(x=6, y=401, height=90)
+base.mainloop()
